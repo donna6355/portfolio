852ba9563d845da4ea30c3a4dfc1c0322d91c9f8
Donna <donnajun1214@gmail.com>
2025-04-12 18:42:49 +0900

    [misc] reference

--- lib/main.dart
+++ lib/main.dart
@@ -22,3 +22,10 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+//https://docs.fastlane.tools/getting-started/android/setup/
+//https://nkstar-ios.tistory.com/entry/Flutter-Fastlane%EC%9C%BC%EB%A1%9C-%EB%B0%B0%ED%8F%AC-%EC%9E%90%EB%8F%99%ED%99%94%EB%A5%BC-%ED%95%B4%EB%B3%B4%EC%9E%90-1
+//1. brew install fastlane
+//2. cd android || ios from root dir
+//3. fastlane init
